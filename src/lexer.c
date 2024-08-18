98a367afa3ea1def60036e72635f1efcf563d346
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-08-18 14:48:34 +0200

    feat: added support for keywords, set default token type to identifier

--- src/lexer.c
+++ src/lexer.c
@@ -5,13 +5,17 @@
 #include "../include/lexer.h"
 #include "../include/token.h"
 
-struct Token make_alloc_failure_token()
+static const size_t KEYWORDS_LENGTH = 2;
+static const char *const KEYWORDS[2] = {"mut", "func"};
+static const char *const TYPES[4] = {"string", "int", "float", "bool"};
+
+static struct Token make_alloc_failure_token()
 {
     return (struct Token){TOKEN_EOF, NULL};
 }
 
 // Function to handle numbers and create a token for them
-struct Token make_number_token(char *chars, size_t *i, size_t length)
+static struct Token make_number_token(char *chars, size_t *i, size_t length)
 {
     /// Initial buffer capacity
     size_t buffer_capacity = 16;
@@ -60,7 +64,70 @@ struct Token make_number_token(char *chars, size_t *i, size_t length)
     {
         token = make_token(TOKEN_INT, buffer);
     }
-    
+
+    free(buffer);
+
+    return token;
+}
+
+static bool is_keyword(char *val)
+{
+    for (size_t i = 0; i < KEYWORDS_LENGTH; i++)
+    {
+        if (strcmp(KEYWORDS[i], val) == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static struct Token make_alpha_token(char *chars, size_t *i, size_t length)
+{
+    /// Initial buffer capacity
+    size_t buffer_capacity = 16;
+    char *buffer = (char *)malloc(buffer_capacity);
+    if (buffer == NULL)
+    {
+        return make_alloc_failure_token();
+    }
+
+    size_t buffer_index = 0;
+
+    while (*i < length && (isalpha((unsigned char)chars[*i]) || chars[*i] == '_'))
+    {
+        // Resize buffer if needed
+        if (buffer_index >= buffer_capacity - 1)
+        {
+            buffer_capacity *= 2;
+            char *new_buffer = (char *)realloc(buffer, buffer_capacity);
+
+            if (new_buffer == NULL)
+            {
+                free(buffer);
+                return make_alloc_failure_token();
+            }
+
+            buffer = new_buffer;
+        }
+
+        buffer[buffer_index++] = chars[(*i)++];
+    }
+
+    buffer[buffer_index] = '\0';
+
+    struct Token token;
+
+    if (is_keyword(buffer))
+    {
+        token = make_token(TOKEN_KEYWORD, buffer);
+    }
+    else
+    {
+        token = make_token(TOKEN_IDENTIFIER, buffer);
+    }
+
     free(buffer);
 
     return token;
@@ -79,7 +146,7 @@ struct Token *lexer(char *chars, size_t length)
     size_t j = 0;
     for (size_t i = 0; i < length; i++)
     {
-        if (chars[i] == '\r' || chars[i] == '\n' || chars[i] == '\0')
+        if (isspace(chars[i]))
         {
             continue;
         }
@@ -87,13 +154,13 @@ struct Token *lexer(char *chars, size_t length)
         if (isdigit((unsigned char)chars[i]))
         {
             tokens[j++] = make_number_token(chars, &i, length);
-            i--; // Adjust `i` because the lexer loop will increment `i` again
         }
         else
         {
-            char buffer[2] = {chars[i], '\0'};
-            tokens[j++] = make_token(TOKEN_STRING, buffer);
+            tokens[j++] = make_alpha_token(chars, &i, length);
         }
+
+        i--; // Adjust `i` because the lexer loop will increment `i` again
     }
 
     tokens[j++] = make_token(TOKEN_EOF, "\0");
